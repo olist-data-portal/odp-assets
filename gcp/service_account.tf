3fb6844e8e0390fc9e551b9ced21ef6ba158c48c
kenichiro.sato <kenichirosato0514@gmail.com>
2025-11-22 13:03:56 +0000

    fix: Remove unsupported labels from service account resource

--- gcp/service_account.tf
+++ gcp/service_account.tf
@@ -2,7 +2,5 @@ resource "google_service_account" "data_pipeline" {
   account_id   = "${var.resource_prefix}-data-pipeline"
   display_name = "Data Pipeline Service Account"
   description  = "データパイプライン実行用サービスアカウント"
-
-  labels = local.common_labels
 }
 
